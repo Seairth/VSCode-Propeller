82fe8cf43ca8fbd12468a66c89804fd82c055ee9
seairth <seairth@gmail.com>
2016-04-30 09:24:21 -0400

    adding basic Spin support, updating p2asm install package

---
+++ spin/src/extension.ts
@@ -0,0 +1,37 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Extension "Spin" is now active!');
+
+    let disposable = vscode.window.onDidChangeActiveTextEditor((editor) => {
+        if(!editor) {
+            return;
+        }
+        
+        if (editor.document.languageId != "spin") {
+            return;
+        }
+        
+        editor.options = getNewOptions(editor.options);
+    });
+    
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+function getNewOptions(oldOptions : vscode.TextEditorOptions) : vscode.TextEditorOptions {
+    return {
+        cursorStyle: oldOptions.cursorStyle,
+        insertSpaces: oldOptions.insertSpaces,
+        tabSize: 8
+    };
+}
